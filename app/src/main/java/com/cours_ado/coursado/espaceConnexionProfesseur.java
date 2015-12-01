ad88afe9a3840cc52d8610c370887baa04c92fe1
Antoine BABU <antoinebabu85@gmail.com>
2015-12-01 11:13:06 +0100

    Ajout Choix prof

--- app/src/main/java/com/cours_ado/coursado/espaceConnexionProfesseur.java
+++ app/src/main/java/com/cours_ado/coursado/espaceConnexionProfesseur.java
@@ -1,7 +1,10 @@
 package com.cours_ado.coursado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class espaceConnexionProfesseur extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class espaceConnexionProfesseur extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_espace_connexion_professeur);
+        Button boutonConnexionProf = (Button) findViewById(R.id.BoutonConnexionProfesseur);
+        boutonConnexionProf.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent connexionProfesseurApprouvée = new Intent(espaceConnexionProfesseur.this,choixProfesseur.class);
+                startActivity(connexionProfesseurApprouvée);
+                espaceConnexionProfesseur.this.finish();
+            }
+        });
     }
 }
