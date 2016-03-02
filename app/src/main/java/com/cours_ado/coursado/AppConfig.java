3bc4c13ebb04ffce907d1a7fd88b822a3f7ed0d8
Antoine BABU <antoinebabu85@gmail.com>
2016-03-02 15:12:57 +0100

    Saisie billet fonctionnel

--- app/src/main/java/com/cours_ado/coursado/AppConfig.java
+++ app/src/main/java/com/cours_ado/coursado/AppConfig.java
@@ -6,4 +6,5 @@ public class AppConfig{
     public final static String URL_Login ="http://10.0.2.2/Cours-Ado/login.php";
     public final static String URL_ListeEleve ="http://10.0.2.2/Cours-Ado/listeEleve.php";
     public final static String URL_ValidateTicket ="http://10.0.2.2/Cours-Ado/validationTicket.php";
+    public final static String URL_SaisirTicket ="http://10.0.2.2/Cours-Ado/saisirBillet.php";
 }
